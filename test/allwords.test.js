e50e72dba59f5d5983bdebf82cd8432a1e8a83cc
spencer kelly <spencerkelly86@gmail.com>
2021-07-14 14:42:54 -0400

    log outdated iana codes

--- test/allwords.test.js
+++ test/allwords.test.js
@@ -3,20 +3,22 @@ import spacetime from 'spacetime'
 import soft from './_lib.js'
 let zones = spacetime().timezones
 import data from '../data/index.js'
-// import current from './_current.js'
+import current from './_current.js'
 
 let list = Object.keys(data)
 test('all words produce valid iana', (t) => {
+  let olds = new Set()
   list.forEach((str) => {
     let found = soft(str)[0] || { iana: '' }
     let id = found.iana.toLowerCase()
     t.ok(found && zones[id], str + ' -> ' + JSON.stringify(found, null, 2))
-    // if (found.offset !== zones[id].offset) {
-    //   console.log(str)
-    //   console.log(found)
-    //   console.log(zones[id])
-    // }
-    // t.equal(found.offset, zones[id].offset, '[offset] ' + str + `  (${id})`)
+
+    if (!current[found.iana]) {
+      olds.add(found.iana)
+    }
+    // ensure we have returned a contemporary iana code
+    t.equal(current[found.iana], true, 'old-tz -' + found.iana)
   })
+  console.log(olds)
   t.end()
 })
