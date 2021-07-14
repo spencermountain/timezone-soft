318abd749f1a8619494c3e2f1ea544f24a389e6c
Michael C <michael@mchang.name>
2021-07-14 00:38:41 -0400

    update IANA list
    list populated with zone.json from https://github.com/mchangrh/tzdb-names/blob/main/dist/zone.json
    
    tzdb 2021a

--- test/allwords.test.js
+++ test/allwords.test.js
@@ -3,19 +3,12 @@ const spacetime = require('spacetime')
 const soft = require('../src')
 let zones = spacetime().timezones
 
-const skip = {
-  'america/fort_wayne': true,
-  'asia/ulan_bator': true,
-  'america/argentina/buenos_aires': true,
-  'asia/tel_aviv': true
-}
-
 let list = Object.keys(require('../data'))
 test('all words produce valid iana', (t) => {
   list.forEach((str) => {
     let found = soft(str)[0] || { iana: '' }
     let id = found.iana.toLowerCase()
-    t.ok((found && zones[id]) || skip[str], str)
+    t.ok(found && zones[id], str)
     // if (found.offset !== zones[id].offset) {
     //   console.log(str)
     //   console.log(found)
